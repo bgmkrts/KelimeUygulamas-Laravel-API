e3068dd5b906960512fef831b8223feca6b51c9b
bgmkrts <34838510+bgmkrts@users.noreply.github.com>
2019-11-08 10:30:13 +0300

    Şifre İşlemleri
    Şifremi unuttum
    Şifremi Yenile

---
+++ app/Models/UserModel.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
+
+class UserModel extends Model
+{
+    use HasApiTokens, Notifiable;
+    protected $table='users';
+    public $timestamps=true;
+    protected $fillable=[
+        'name',
+        'surname',
+        'password',
+        'user_name',
+        'email'
+    ];
+
+
+}
---
+++ app/Models/WordModel.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class WordModel extends Model
+{
+    protected $table='words';
+    public $timestamps=true;
+    protected $fillable=[
+        'word_tr',
+        'word_eng',
+        'degreeOfDifficulty'
+    ];
+}
+
